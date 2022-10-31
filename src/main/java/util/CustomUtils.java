bd30a50e1ef5083c4ae15920e5ac34a639df4793
honinbo6355 <honinbo6355@nate.com>
2022-10-31 18:56:07 +0900

    요구사항 2, 3 완료

--- src/main/java/util/CustomUtils.java
+++ src/main/java/util/CustomUtils.java
@@ -1,11 +1,40 @@
 package util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CustomUtils {
-    public static String getRequestPath(String header) {
-        if (header == null) {
+    public static Map<String, String> getRequestPath(String line) {
+        if (line == null) {
             return null;
         }
+        Map<String, String> map = new HashMap<>();
+        String[] tokens = line.split(" ");
 
-        return header.split(" ")[1];
+        map.put("method", tokens[0]);
+
+        if (tokens[1].contains("?")) {
+            String[] url = tokens[1].split("[?]");
+
+            map.put("path", url[0]);
+            map.put("queryString", url[1]);
+        } else {
+            map.put("path", tokens[1]);
+            map.put("queryString", null);
+        }
+
+        return map;
+    }
+
+    public static Map<String, String> getHeaderInfo(String line) {
+        if (line == null) {
+            return null;
+        }
+        Map<String, String> map = new HashMap<>();
+        String[] tokens = line.split(" ");
+
+        map.put(tokens[0], tokens[1]);
+
+        return map;
     }
 }
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,16 +1,16 @@
 package webserver;
 
-import java.io.*;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.net.SocketAddress;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.CustomUtils;
+import util.HttpRequestUtils;
+import util.IOUtils;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Map;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -27,11 +27,18 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            String requestPath = CustomUtils.getRequestPath(br.readLine());
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "utf-8"));
+            String line = br.readLine();
+            if (line == null) {
+                return;
+            }
+            Map<String, String> requestPath = CustomUtils.getRequestPath(line);
 
 //            while (true) {
 //                line = br.readLine();
+//                if (line == null) {
+//                    return;
+//                }
 //                if ("".equals(line)) {
 //                    log.debug("header empty : {}", line);
 //                    break;
@@ -39,11 +46,39 @@ public class RequestHandler extends Thread {
 //                log.debug("header : {}", line);
 //            }
 
-            if ("/index.html".equals(requestPath)) {
-                byte[] body = Files.readAllBytes(new File("./webapp" + requestPath).toPath());
+            if ("/index.html".equals(requestPath.get("path")) || "/user/form.html".equals(requestPath.get("path"))) {
+                byte[] body = Files.readAllBytes(new File("./webapp" + requestPath.get("path")).toPath());
                 DataOutputStream dos = new DataOutputStream(out);
                 response200Header(dos, body.length);
                 responseBody(dos, body);
+            } else if ("/user/create".equals(requestPath.get("path"))) {
+                if ("GET".equals(requestPath.get("method"))) {
+                    Map<String, String> queryString = HttpRequestUtils.parseQueryString(requestPath.get("queryString"));
+
+                    User user = new User(queryString.get("userId"), queryString.get("password"), queryString.get("name"), queryString.get("email"));
+                    System.out.println(user);
+                } else if ("POST".equals(requestPath.get("method"))) {
+                    int contentLength = 0;
+
+                    while (true) {
+                        line = br.readLine();
+                        if (line == null) {
+                            return;
+                        }
+                        if ("".equals(line)) {
+                            break;
+                        }
+
+                        Map<String, String> headerInfo = CustomUtils.getHeaderInfo(line);
+
+                        if (headerInfo.containsKey("Content-Length:")) {
+                            contentLength = Integer.parseInt(headerInfo.get("Content-Length:"));
+                        }
+                    }
+                    Map<String, String> body = HttpRequestUtils.parseQueryString(IOUtils.readData(br, contentLength));
+                    User user = new User(body.get("userId"), body.get("password"), body.get("name"), body.get("email"));
+                    System.out.println(user);
+                }
             }
         } catch (IOException e) {
             log.error(e.getMessage());
