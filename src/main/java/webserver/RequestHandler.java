865481201687cd9b6f8b62c91b8f455d292a6487
seongchan.hwang <seongchan.hwang@navercorp.com>
2022-11-02 01:02:02 +0900

    [요구사항 5] login

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -10,10 +10,10 @@ import io.HttpMessageReader;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+    public static final String FILE_BASE_PATH = "./webapp";
 
     private Socket connection;
 
@@ -41,6 +41,10 @@ public class RequestHandler extends Thread {
                     handleSignUp(requestReader.readBodyMap(), out);
                     break;
                 }
+                case "/user/login": {
+                    handleLogin(requestReader.readBodyMap(), out);
+                    break;
+                }
                 default: {
                     handleDefault(out);
                 }
@@ -48,6 +52,23 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private void handleLogin(Map<String, String> body, OutputStream out) throws IOException {
+        String userId = body.get("userId");
+        User user = DataBase.findUserById(userId);
+
+        DataOutputStream dos = new DataOutputStream(out);
+        if (user == null) {
+            byte[] file = Files.readAllBytes(new File(FILE_BASE_PATH + "/user/login_failed.html").toPath());
+            response200HeaderWithCookie(dos, file.length, false);
+            responseBody(dos, file);
+            return;
+        }
+
+        byte[] file = Files.readAllBytes(new File(FILE_BASE_PATH + "/index.html").toPath());
+        response200HeaderWithCookie(dos, file.length, true);
+        responseBody(dos, file);
     }
 
     private void handleDefault(OutputStream out) {
@@ -60,7 +81,7 @@ public class RequestHandler extends Thread {
     private void handleFileRequest(String url, OutputStream out) throws IOException {
         try {
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+            byte[] body = Files.readAllBytes(new File(FILE_BASE_PATH + url).toPath());
             response200Header(dos,body.length);
             responseBody(dos, body);
         } catch (IOException e) {
@@ -94,10 +115,22 @@ public class RequestHandler extends Thread {
         }
     }
 
+    // Cookie 클래스 정의하면 좋을듯.
+    private void response200HeaderWithCookie(DataOutputStream dos, int lengthOfBodyContent, boolean cookie) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("Set-Cookie: logined=" + cookie + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
     private void response302Header(DataOutputStream dos) {
         try {
             dos.writeBytes("HTTP/1.1 302 Found \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Location: http://localhost:8080/index.html\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
