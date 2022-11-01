87970a6bc0061a20756fd4d860e36c9549ed3f4d
inhalin <inhalin.kim@gmail.com>
2022-11-02 01:46:39 +0900

    요구사항 2 - get 방식으로 회원가입

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,12 +1,15 @@
 package webserver;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.Map;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -29,21 +32,24 @@ public class RequestHandler extends Thread {
             if (line == null) {
                 return;
             }
-            String url = getUrl(line);
-            log.debug("url = {}", url);
+
+            String[] tokens = line.split(" ");
+            String url = getUrl(tokens[1]);
+            Map<String, String> params = getParams(tokens[1]);
 
             while (!"".equals(line)) {
                 line = br.readLine();
             }
 
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-
-            if (url != null) {
-                body = Files.readAllBytes(new File("./webapp" + url).toPath());
+            if (params != null) {
+                User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+                log.debug("user = {}", user);
+            } else {
+                DataOutputStream dos = new DataOutputStream(out);
+                byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+                response200Header(dos, body.length);
+                responseBody(dos, body);
             }
-            response200Header(dos, body.length);
-            responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
@@ -69,11 +75,23 @@ public class RequestHandler extends Thread {
         }
     }
 
-    private String getUrl(String line) {
-        String url = line.split(" ")[1];
+    private String getUrl(String token) {
+        String url = token;
 
-        if (url.length() > 1) {
-            return url;
+        if (url.contains("?")) {
+            int index = url.indexOf("?");
+            url = url.substring(0, index);
+        }
+
+        return url;
+    }
+
+    private Map<String, String> getParams(String token) {
+        if (token.contains("?")) {
+            int index = token.indexOf("?") + 1;
+            String params = token.substring(index);
+
+            return HttpRequestUtils.parseQueryString(params);
         }
 
         return null;
